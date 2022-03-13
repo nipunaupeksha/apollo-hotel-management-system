8a33d84b576f09c09d7c3bf7aa4a63886001c3c6
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-03-13 19:10:13 +0530

    creating dashboard

---
+++ src/main/java/com/apollo/hotel/ApolloHotelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.apollo.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloHotelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloHotelManagementApplication.class, args);
+	}
+
+}
